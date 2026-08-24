e82f7947fc35826ca11b9566ef710683b8e92f62
dev-penniel <tlhonolofatsopennielmothae@gmail.com>
2026-08-24 16:34:39 +0200

    seed and crud functionality for  permissions

---
+++ resources/views/pages/roles/⚡create.blade.php
@@ -0,0 +1,88 @@
+<?php
+
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+use Livewire\Component;
+
+new class extends Component {
+
+    public $permissions, $name;
+    public $selectedPermissions = [];
+
+    public function mount(): Void
+    {
+        $this->permissions = Permission::all();
+    }
+
+    public function createRole(){
+
+        $validated = $this->validate([
+            'name' => ['string', 'required'],
+        ]);
+
+
+        $role = Role::create($validated);
+
+        $role->syncPermissions($this->selectedPermissions);
+
+        $this->dispatch('role-created');
+
+    }
+
+}; ?>
+
+<div>
+    <div>
+        <div class="relative mb-6 w-full">
+            <div class="flex justify-between items-center">
+                <div>
+                    <div class="flex gap-2 items-center">
+                        {{-- <a wire:navigate href="{{ route('roles.index') }}"><flux:icon.arrow-left-circle /></a> --}}
+                        <flux:heading size="xl" level="1">{{ __('Add Role') }}</flux:heading>
+                    </div>
+                    <flux:breadcrumbs class="mb-4 mt-2">
+                        <flux:breadcrumbs.item href="{{ route('dashboard') }}">Home</flux:breadcrumbs.item>
+                        <flux:breadcrumbs.item href="{{ route('dashboard') }}">Roles</flux:breadcrumbs.item>
+                        <flux:breadcrumbs.item >Create</flux:breadcrumbs.item>
+                    </flux:breadcrumbs>
+                </div>
+            </div>
+            <flux:separator variant="subtle" />
+        </div>
+        <form wire:submit.prevent="createRole" >
+            <div class="flex gap-5 mb-5">
+                <flux:input
+                    wire:model="name"
+                    :label="__('Name')"
+                    type="text"
+                    required
+                    placeholder="Name"
+                    autocomplete="name"
+                />
+
+
+                
+            </div>
+
+            <div class="space-x-2">
+                @foreach ($permissions as $permission )
+                    <label for="" ><input wire:model="selectedPermissions" value="{{ $permission->name }}" type="checkbox"> {{ $permission->name }}</label>
+                @endforeach
+            </div>
+
+            <div class="flex items-center gap-4">
+                <div class="flex items-center justify-end">
+                    <flux:button variant="primary" type="submit" class="w-full">{{ __('Save') }}</flux:button>
+                </div>
+    
+                <div
+                    x-data="{ show: false }"
+                    x-on:role-created.window="show = true; setTimeout(() => show = false, 3000)"
+                >
+                    <span x-show="show" x-transition>
+                        {{ __('Saved.') }}
+                    </span>
+                </div>
+            </div>
+        </form>
+    </div>
--- routes/web.php
+++ routes/web.php
@@ -8,4 +8,13 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::view('dashboard', 'dashboard')->name('dashboard');
 });
 
+Route::livewire('/roles', 'pages::roles.index' )->name('roles');
+Route::livewire('/roles/create', 'pages::roles.create' )->name('roles.create');
+Route::livewire('/role/{id}', 'pages::roles.edit' )->name('roles.edit');
+
+Route::livewire('/users', 'pages::users.index' )->name('roles');
+Route::livewire('/users/create', 'pages::users.create' )->name('roles.create');
+Route::livewire('/user/{id}', 'pages::user.edit' )->name('roles.edit');
+
+
 require __DIR__.'/settings.php';
