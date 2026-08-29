81e68c6eeb75f5d151ca9612d50bb65a59ea3755
dev-penniel <tlhonolofatsopennielmothae@gmail.com>
2026-08-30 01:43:08 +0200

    feature: custom auth pages

--- resources/views/layouts/auth/split.blade.php
+++ resources/views/layouts/auth/split.blade.php
@@ -4,9 +4,17 @@
         @include('partials.head')
     </head>
     <body class="min-h-screen bg-white antialiased dark:bg-linear-to-b dark:from-neutral-950 dark:to-neutral-900">
-        <div class="relative grid h-dvh flex-col items-center justify-center px-8 sm:px-0 lg:max-w-none lg:grid-cols-2 lg:px-0">
+        <div class=" dark:bg-zinc-900 relative grid h-dvh flex-col items-center justify-center px-8 sm:px-0 lg:max-w-none lg:grid-cols-2 lg:px-0">
             <div class="bg-muted relative hidden h-full flex-col p-10 text-white lg:flex dark:border-e dark:border-neutral-800">
-                <div class="absolute inset-0 bg-neutral-900"></div>
+
+                <div class="absolute inset-0">
+                    <img
+                        src="{{ asset('storage/app-images/wallpaper.jpg') }}"
+                        alt=""
+                        class="h-full w-full object-cover"
+                    >
+                </div>
+                
                 <a href="{{ route('home') }}" class="relative z-20 flex items-center text-lg font-medium" wire:navigate>
                     <span class="flex h-10 w-10 items-center justify-center rounded-md">
                         <x-app-logo-icon class="me-2 h-7 fill-current text-white" />
@@ -25,7 +33,7 @@
                     </blockquote>
                 </div>
             </div>
-            <div class="w-full lg:p-8">
+            <div class="w-full lg:p-8 ">
                 <div class="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
                     <a href="{{ route('home') }}" class="z-20 flex flex-col items-center gap-2 font-medium lg:hidden" wire:navigate>
                         <span class="flex h-9 w-9 items-center justify-center rounded-md">
